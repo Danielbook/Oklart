49f4bac967b153ce4ab89409d4a6c1bc86ad37a8
Adaso <adaso578@student.liu.se>
2016-02-01 18:05:45 +0100

    Added transport layer

--- karta.js
+++ karta.js
@@ -1,12 +1,37 @@
+
+
+var osmLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+var osmLayer2 = new ol.layer.Tile({
+  source: new ol.source.MapQuest({layer: 'osm'})
+});
+
+var Stamen = new ol.layer.Tile({
+            source: new ol.source.Stamen({
+              layer: 'watercolor'
+            })
+          });
+
+//http://wiki.openstreetmap.org/wiki/Tile_servers
+var openTransportMapLayer = new ol.layer.Tile({
+  source: new ol.source.OSM({
+    url: 'http://{a-b}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png'
+  })
+});
+
 var map = new ol.Map({
-        target: 'map',
+        target: 'map', //Attach map to 'map' div
+       
         layers: [
-          new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'sat'})
-          })
+          openTransportMapLayer
         ],
+
         view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
-      });
+          center: ol.proj.fromLonLat([16.192420999999968, 58.587745]),
+          zoom: 4,
+          maxZoom: 6,
+          minZoom: 4
+      })
+});
