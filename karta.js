e1297770dc3097ef60d68d14941c899ed1b46923
Adaso <adaso578@student.liu.se>
2016-02-01 14:50:39 +0100

    Added map

---
+++ karta.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.MapQuest({layer: 'sat'})
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
