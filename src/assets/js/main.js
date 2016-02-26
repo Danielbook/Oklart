7d87249fb3f164a9d4305d17ba579c2dcf16fa64
ejuopperi <juopperiemil@gmail.com>
2016-02-26 10:03:34 +0100

    Implement module pattern in graph and lift out google.chart.loag to index file

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,24 +19,17 @@ require([
   /**
    * 
    */
-   //console.log(Table.drawTable());
-	
-	
   function getData(dataurl){
     return $.getJSON(dataurl).then(function(data){
         return data;
       })
     ;}
 
-  google.charts.load('current', {packages: ['corechart', 'table', 'line']});
+
 	getData("./assets/data/data.json").then(function(returndata){ 
     console.log(returndata);
     Table.initTable(returndata);
-    google.charts.setOnLoadCallback(drawGraph);
+    Graph.initGraph(returndata);
     createMap();
-    //console.log(returndata);
-    // google.chars.setOnLoadCallback(Table.drawTable());
-    // google.charts.setOnLoadCallback(drawGraph);
-    // createMap();
   });
 });
