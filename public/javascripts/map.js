84e28b4dc27497e7e1e0ba8027b8610ea3e4ca03
Utanforskapet <jelmesioo@gmail.com>
2016-03-02 10:29:47 +0100

    Test

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -10,24 +10,24 @@ define(['map'], function (map) {
 
   Map.prototype.initMap = function(smhidata) {
 
-var cartoDBLight = new ol.layer.Tile({
-    source: new ol.source.OSM({
+    var cartoDBLight = new ol.layer.Tile({
+      source: new ol.source.OSM({
       url: 'http://{a-b}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png' //Tile server
     })
-  });
+    });
 
   //Bounding box
   var extent = ol.proj.transformExtent([2.25, 52.5, 38.00, 70.75], 'EPSG:4326', 'EPSG:3857');
   var view = new ol.View({
     center: ol.proj.fromLonLat([15.380859, 62.160372]), //Mitt i sverige
     zoom: 4,
-      maxZoom: 10,
-      minZoom: 4,
+    maxZoom: 10,
+    minZoom: 4,
       //extent: extent
     })
 
 
-     /* -------- Marker layer -------- */
+  /* -------- Marker layer -------- */
   var currPosVectorSource = new ol.source.Vector({});
   var currPosVectorLayer = new ol.layer.Vector({
     source: currPosVectorSource
@@ -53,7 +53,7 @@ var cartoDBLight = new ol.layer.Tile({
       })
     }).extend([
    //   new app.RotateNorthControl()
-    ]),
+   ]),
 
     layers: [
     cartoDBLight,
@@ -62,7 +62,6 @@ var cartoDBLight = new ol.layer.Tile({
     view: view
     
   });
-
 
 
   /* -------------- Gelocation ------------------- */
@@ -74,15 +73,15 @@ var cartoDBLight = new ol.layer.Tile({
 
   if(geolocation){
     console.log("Succes!");
-  
+    
   //create a vector source to add the icon(s) to.
 
   geolocation.once('change', function(evt) {
    //save position and set map center
-    pos = geolocation.getPosition();
-    map.getView().setCenter(ol.proj.fromLonLat(pos));
+   pos = geolocation.getPosition();
+   map.getView().setCenter(ol.proj.fromLonLat(pos));
 
-  
+   
    //create icon at new map center
    var iconFeature = new ol.Feature({
      geometry: new ol.geom.Point(ol.proj.fromLonLat(pos)), 
@@ -91,66 +90,66 @@ var cartoDBLight = new ol.layer.Tile({
 
    //iconFeature.setStyle(markerIconStyle);
    //currPosVectorSource.addFeature(iconFeature);    
-      
+   
  });
 
-  }
-  else {
-    alert("Couldn't find location");
-  }
-  /*---------------------------------------------------*/
+}
+else {
+  alert("Couldn't find location");
+}
+/*---------------------------------------------------*/
 
 
- document.getElementById("SearchBtn").onclick = function(){
+document.getElementById("SearchBtn").onclick = function(){
 
-    var pan = ol.animation.pan({
-      duration: 1000,
-      source: view.getCenter()
-    });
+  var pan = ol.animation.pan({
+    duration: 1000,
+    source: view.getCenter()
+  });
 
-     var AdresFalt = $("#CitySearch") ;
-       $.getJSON('http://nominatim.openstreetmap.org/search?format=json&q=' + AdresFalt.val(), function(data) {
-            var FoundExtent = data[0].boundingbox;
-            var placemark_lat = data[0].lat;
-            var placemark_lon = data[0].lon;
-          
-          map.beforeRender(pan);        
-          map.getView().setCenter(ol.proj.transform([Number(placemark_lon), Number(placemark_lat)], 'EPSG:4326', 'EPSG:3857'));
-        
-       }); 
-    }   
+  var AdresFalt = $("#CitySearch") ;
+  $.getJSON('http://nominatim.openstreetmap.org/search?format=json&q=' + AdresFalt.val(), function(data) {
+    var FoundExtent = data[0].boundingbox;
+    var placemark_lat = data[0].lat;
+    var placemark_lon = data[0].lon;
+    
+    map.beforeRender(pan);        
+    map.getView().setCenter(ol.proj.transform([Number(placemark_lon), Number(placemark_lat)], 'EPSG:4326', 'EPSG:3857'));
+    
+  }); 
+}   
 
 
 $("#CitySearch").keypress(function(e){
-    if(e.which == 13){
+  if(e.which == 13){
     var pan = ol.animation.pan({
       duration: 1000,
       source: view.getCenter()
     });
 
-     var AdresFalt = $("#CitySearch") ;
-       $.getJSON('http://nominatim.openstreetmap.org/search?format=json&q=' + AdresFalt.val(), function(data) {
-            var FoundExtent = data[0].boundingbox;
-            var placemark_lat = data[0].lat;
-            var placemark_lon = data[0].lon;
-          
-          map.beforeRender(pan);        
-          map.getView().setCenter(ol.proj.transform([Number(placemark_lon), Number(placemark_lat)], 'EPSG:4326', 'EPSG:3857'));
-        
-       }); 
-    }
-  });
+    var AdresFalt = $("#CitySearch") ;
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&q=' + AdresFalt.val(), function(data) {
+      var FoundExtent = data[0].boundingbox;
+      var placemark_lat = data[0].lat;
+      var placemark_lon = data[0].lon;
+      
+      map.beforeRender(pan);        
+      map.getView().setCenter(ol.proj.transform([Number(placemark_lon), Number(placemark_lat)], 'EPSG:4326', 'EPSG:3857'));
+      
+    }); 
+  }
+});
 
 /* ------ Autocomplete ------ */
 var input = document.getElementById('CitySearch');
 var options = {};
-    
+
 new google.maps.places.Autocomplete(input, options);
 
 /* ---------------------------*/
 
-  }
+}
 
 
-  return Map;
+return Map;
 });
