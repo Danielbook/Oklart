2c2ccb19ea5bc71dabe09c443e77b4e160504036
ejuopperi <juopperiemil@gmail.com>
2016-03-11 09:51:41 +0100

    Data for each element in locations.json are loaded from SMHI using the async module

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,11 @@ var http = require('http');
 var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
 var locations = require('../locations.json');
+var async = require("async");
 
 //If data in DB is old, refresh the data
 if(true){
-	refreshDB(locations);
+	asyncTest(locations);
 }else {
 	readDB();
 }
@@ -15,51 +16,52 @@ if(true){
 
 
 function refreshDB(locations){
+	var urls = [];
+
+	for(var i = 0; i < locations.length; i++){
+		urls[i] = "http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/" + locations[i].lat + "/lon/" + locations[i].lon + "/data.json";
+	}
 
 	var db = MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 		if(err)
 	        throw err;
 	    myCollection = db.collection('data');
 	    myCollection.remove({});
-	    var counter = 0;
 
-		for(var i = 0; i < locations.length; i++){
+		// 1st para in async.each() is the array of urls
+		async.each(urls,
+	  	  // 2nd param is the function that each item is passed to
+		  function(url, callback){
+	      // Call the http get async function and call callback when one datapoint has been inserted in db
+		    http.get(url, function(res){
+		    var body = '';
 
-			console.log("Har skickat förfrågan om data");
-			url = "http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/" + locations[i].lat + "/lon/" + locations[i].lon + "/data.json";
-			addDataPoint(url);
-			counter++;
-		}
-	})
-};
+			    res.on('data', function(chunk){
+			        body += chunk;
+			    });
 
-function addDataPoint(url){
-	//Get data from smhi-api
-	http.get(url, function(res){
-    var body = '';
+			    res.on('end', function(){
+			    	var smhiResponse = JSON.parse(body);
+			    	//Add datapoint to db
+				    myCollection.insert(smhiResponse, function(err, result) {
+					    if(err)
+					        throw err;
 
-	    res.on('data', function(chunk){
-	        body += chunk;
-	    });
-
-	    var counter = 0;
-	    res.on('end', function(){
-	    	var smhiResponse = JSON.parse(body);
-
-	    	//Add datapoint to db
-		    myCollection.insert(smhiResponse, function(err, result) {
-			    if(err)
-			        throw err;
+						callback();
+					})
+				})
 			})
-
-			console.log("Har hämtat data");
-
+		  },
+		  // 3rd param is the function to call when everything's done
+		  function(err){
+		  	console.log("All data har hämtats");
 		    readDB();
-		})
+		  }
+		);
 	})
+}
 
-};
-
+//Read DB and render index.ejs with data from collection data
 function readDB(){
 var db = MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 		    if(err)
