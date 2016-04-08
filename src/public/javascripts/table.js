383ce9cc479248182da990780a40b1783fad8f08
Daniel Böök <Daniel.book@hotmail.com>
2016-04-08 13:00:08 +0200

    Klar, ska mergeas med master

--- src/public/javascripts/table.js
+++ src/public/javascripts/table.js
@@ -5,45 +5,7 @@ define(['table'], function (table) {
   */
   var Table = function(smhidata) {
   };
-
-  Table.prototype.precipitationType = function(pcat) {
-    switch (pcat){
-      case 0:{
-        return "Ingen nederbörd";
-      }
-      case 1:{
-        return "Snö";
-      }
-      case 2:{
-        return "Snö och Regn";
-      }
-      case 3:{
-        return "Regn";
-      }
-      case 4:{
-        return "Duggregn";
-      }
-      case 5:{
-        return "Underkylt regn";
-      }
-      case 6:{
-        return "Underkylt duggregn";
-      }
-      default:{
-        return "";
-      }
-    }
-  };
-
-  Table.prototype.dateAndTime = function(validTime) {
-    return validTime.substring(0,10) + " " + validTime.substring(11,16);
-  };
-
-  Table.prototype.testFunction = function () {
-    console.log("test");
-  };
-
-
+  
   Table.prototype.initTable = function(arg) {
     this._data = google.charts.setOnLoadCallback(this.drawTable(arg));
   };
@@ -55,9 +17,6 @@ define(['table'], function (table) {
       data.addColumn('string', 'Tid');
       data.addColumn('number', 'Väder');
       data.addColumn('number', 'Vindstyrka');
-      data.addColumn('number', 'Nederbörd');
-
-      console.log(smhidata[0].timeseries[0]);
 
       for(var i = 0; i < 30; i++) {
 
@@ -71,10 +30,6 @@ define(['table'], function (table) {
           {
             v: smhidata[0].timeseries[i].gust,
             f: smhidata[0].timeseries[i].gust.toString() + "m/s"
-          },
-          {
-            v: smhidata[0].timeseries[i].pcat,
-            f: this.precipitationType(smhidata[0].timeseries[i].pcat)
           }
           ]
         ]);
