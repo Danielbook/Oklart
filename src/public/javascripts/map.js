57272de791a70a7f3af12a66986534c0dbb72fd3
ejuopperi <juopperiemil@gmail.com>
2016-04-25 14:57:04 +0200

    cool

--- src/public/javascripts/map.js
+++ src/public/javascripts/map.js
@@ -274,6 +274,7 @@ define([
           for(var idx=0; idx < that._data.length; idx++){
               if( String(that._data[idx].name) == String(feature.getStyle().getText().getText())){
                 updateLocation(idx,'t',0);
+                updateTable(user.time, idx);
                 dataObject=that._data[idx];
               }
           }
